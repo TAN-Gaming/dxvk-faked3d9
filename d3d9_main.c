e8954a0c2195015d8e8468d40a233edaba239c8d
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-03 14:22:43 +0700

    Update d3d9_main.c
    Add CC0 notice.

--- d3d9_main.c
+++ d3d9_main.c
@@ -1,3 +1,20 @@
+/*
+ * dxvk-faked3d9
+ *
+ * A dummy d3d9.dll to fix launching problems for some d3d10 and d3d11 games
+ * running with DXVK.
+ *
+ * Written in 2022 by Thamatip Chitpong <tangaming123456@outlook.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 #include <windows.h>
 #include <d3d9.h>
 
@@ -36,3 +53,4 @@ void WINAPI D3DPERF_SetMarker(D3DCOLOR col, LPCWSTR wszName) { }
 void WINAPI D3DPERF_SetOptions(DWORD dwOptions) { }
 
 void WINAPI D3DPERF_SetRegion(D3DCOLOR col, LPCWSTR wszName) { }
+
