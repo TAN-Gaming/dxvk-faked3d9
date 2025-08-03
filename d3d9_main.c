4fa9942113a039ee30bd22dca8aa0e0d030123b0
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-04 00:30:16 +0700

    Change license to 0BSD

--- d3d9_main.c
+++ d3d9_main.c
@@ -1,16 +1,8 @@
 /*
- * dxvk-faked3d9 - A dummy d3d9.dll to fix launching problems for some d3d10
- * and d3d11 games running with DXVK.
+ * Author: Thamatip Chitpong, 2022
  *
- * Written in 2022 by Thamatip Chitpong <tangaming123456@outlook.com>
- *
- * To the extent possible under law, the author(s) have dedicated all copyright
- * and related and neighboring rights to this software to the public domain
- * worldwide. This software is distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication along
- * with this software. If not, see
- * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ * SPDX-License-Identifier: 0BSD
+ * License-Filename: LICENSE.txt
  */
 
 #include <windows.h>
